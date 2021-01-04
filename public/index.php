0d6718974284f758987f20ddb37f08c0e961c2b5
Draixer <74540317+Draixer@users.noreply.github.com>
2021-01-04 14:47:26 +0100

    Basic Page mit leicht kopiertem Stylesheet.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,29 @@
-<html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 <link rel="shortcut icon" type="image/x-icon" href="./3475favicon.ico">
-<?php
-  echo 'Hello World';
-?>
+<link rel="stylesheet" href="stylesheet.css">
+  <head>
+
+    <meta charset="utf-8">
+    <title>Schmeißrein</title>
+
+  </head>
+  <body>
+    <div class="background">
+    <header>
+
+    </header>
+    <main>
+
+    <div class="title">Schmeißrein</div>
+    <div class="msg">Wilkommen auf Schmeißrein.
+      Auf dieser Seite kannst du einfach deine Zutaten einwerfen und es werden dir viele tolle Rezepte angezeigt. </div>
+    </main>
+
+    <footer>
+
+    </footer>
+
+  </body>
+
 </html>
