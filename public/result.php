f27efcae314b8b0c657c9b584d61fc2cbc4d489a
Draixer <74540317+Draixer@users.noreply.github.com>
2021-01-05 17:03:51 +0100

    Result Page und extra style

--- public/result.php
+++ public/result.php
@@ -1,5 +1,30 @@
-<?php
-  include "../private/crawl.php";
-  crawl();
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<link rel="shortcut icon" type="image/x-icon" href="./3475favicon.ico">
+<link rel="stylesheet" href="stylesheet1.css">
+  <head>
 
- ?>
+    <meta charset="utf-8">
+    <title>Schmeißrein</title>
+
+  </head>
+  <body>
+    <div class="background">
+    <header>
+
+      <div class="title">Schmeißraus</div>
+
+    </header>
+    <main>
+
+      <?php
+        include "../private/crawl.php";
+        crawl();
+
+       ?>
+
+    </main>
+
+  </body>
+
+</html>
